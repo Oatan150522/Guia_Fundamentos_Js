0d5a402ab4e4a5065d97acc84d6d99f15bbc1ea3
Jhonatan David leherici <lehericijhonathandavid@gmail.com>
2025-11-18 17:07:08 -0500

    Conceptos Basicos de JavaScrip

---
+++ app.js
@@ -0,0 +1,63 @@
+/*1. Saludo personalizado
+let Nombre = prompt("Porfavor Digite su nombre: ")
+console.log("Hola" + Nombre)
+*/
+
+
+/*2. Area de un rectangulo
+let A = parseInt(prompt("Ingrese la altura: "));
+let B= parseInt(prompt("Ingrese la Base: "));
+let Resultado = A*B;
+alert("El resultado es: "+ Resultado);
+*/
+
+
+/*3. Promedio de tres notas
+let N1 = parseInt(prompt("Ingresa el N1: "));
+let N2 = parseInt(prompt("Ingrese el N2: "));
+let N3 = parseInt(prompt("Ingrese el N3: "));
+let Suma = ( N1 + N2 + N3) 
+let Promedio= Suma/ 3
+alert("El resultado es: " + Promedio)
+*/
+
+
+/*4. Conversion de monedas
+let Valor = parseInt(prompt("Digite el valor en pesos colombeianos: "));
+let Resultado = Valor/4000;
+alert ("El resultado es: " + Resultado)
+*/
+
+
+/*5. Conversion de temperatura
+let Celsius = parseInt(prompt("Digite la temperatura en Cº: "));
+const  Fahrenheit= (Celsius * 9/5) + 32;
+alert("La conversion a Fahrenheit es de: " + Fahrenheit);
+*/
+
+
+/*6. Calcular perimetro
+let Lado = parseInt(prompt("Ingrese la longitud del lado: "));
+let Resultado = Lado*Lado;
+alert("El perimetro es: " + Resultado)
+*/
+
+
+// Calcular doble o tiple 
+let Num = parseInt(prompt("Ingrese el numero: "));
+
+let doble = Num * 2;
+let Tiple = Num * 3;
+
+alert(`EL doble de ${Num} es: ${doble} y el tiple de ${Num} es: ${Tiple}   `)
+
+
+
+
+
+
+
+
+
+
+
